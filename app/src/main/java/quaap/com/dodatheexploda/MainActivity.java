c59b53bf4646cc7a566eef3811c1fb965b4f5d40
quaap <kliethermes.tom@gmail.com>
2017-03-11 09:56:01 -0600

    import basic

---
+++ app/src/main/java/quaap/com/dodatheexploda/MainActivity.java
@@ -0,0 +1,167 @@
+package quaap.com.dodatheexploda;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private FrameLayout mMainScreen;
+    private LinearLayout mLookFors;
+
+
+    private List<TextView> activeSyms = new ArrayList<>();
+    private int current = -1;
+    private TextView currentWid = null;
+    private TextView currentLookForWid = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMainScreen = (FrameLayout) findViewById(R.id.main_screen);
+        mLookFors = (LinearLayout) findViewById(R.id.look_for);
+
+        mMainScreen.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                start();
+            }
+        }, 100);
+
+    }
+
+
+    private void showNext() {
+        if (currentLookForWid!=null) {
+            currentLookForWid.setBackgroundColor(Color.DKGRAY);
+            currentLookForWid.setTextColor(Color.GRAY);
+            currentLookForWid.setTextSize(28);
+        }
+        if (currentWid!=null) {
+            mMainScreen.removeView(currentWid);
+        }
+
+        current++;
+        currentWid = activeSyms.get(current);
+        String sym = (String)currentWid.getTag();
+
+        currentWid.setOnClickListener(this);
+
+
+        currentLookForWid = new TextView(this);
+        currentLookForWid.setText(sym);
+        currentLookForWid.setTextSize(50);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        lp.setMarginEnd(8);
+        lp.setMarginStart(8);
+
+        mLookFors.addView(currentLookForWid, 0, lp);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String symv = (String)v.getTag();
+
+        TextView wid2 = activeSyms.get(current);
+        String symw = (String)wid2.getTag();
+
+        if (symv.equals(symw)) {
+            Log.d("Doda", "Found " + symv);
+            showNext();
+        }
+
+    }
+
+    private void start() {
+        int num = 40;
+        Set<Integer> actives = new HashSet<>();
+        mMainScreen.removeAllViews();
+        mLookFors.removeAllViews();
+        activeSyms.clear();
+
+
+        for (int j = 0; j < num; j++) {
+
+            int symind;
+            do {
+              symind = getInt(syms.length);
+            } while (actives.contains(symind));
+            actives.add(symind);
+
+            String sym = syms[symind];
+
+            TextView wid2 = addSymToScreen(sym);
+            activeSyms.add(0, wid2);
+
+        }
+
+
+        showNext();
+    }
+
+    private TextView addSymToScreen(String sym) {
+        TextView wid2 = new TextView(this);
+        wid2.setTag(sym);
+        wid2.setText(sym);
+        wid2.setTextSize(getInt(20)+30);
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(getInt(mMainScreen.getWidth()-70), getInt(mMainScreen.getHeight()-70), 0 ,0);
+        lp.gravity = Gravity.START | Gravity.TOP;
+        wid2.setLayoutParams(lp);
+        mMainScreen.addView(wid2, lp);
+        return wid2;
+    }
+
+    private static int getInt(int max) {
+        return (int)(Math.random()*max);
+
+    }
+
+    public final static String[] syms;
+    private static final int[] symsHex = {
+            0x263A, 0x2615, 0x26BE, 0x26F5, 0x26BD, 0x1F680, 0x1F308, 0x1F332, 0x1F333, 0x1F34B,
+            0x1F350, 0x1F37C, 0x1F3C7, 0x1F3C9, 0x1F3E4, 0x1F400, 0x1F401, 0x1F402, 0x1F403,
+            0x1F404, 0x1F405, 0x1F406, 0x1F407, 0x1F408, 0x1F409, 0x1F40A, 0x1F40B, 0x1F40F,
+            0x1F410, 0x1F413, 0x1F415, 0x1F416, 0x1F42A, 0x1F600, 0x1F607, 0x1F608, 0x1F60E,
+            0x1F525, 0x1F526, 0x1F527, 0x1F528, 0x1F529, 0x1F52E, 0x1F530, 0x1F531, 0x1F4DA,
+            0x1F498, 0x1F482, 0x1F483, 0x1F484, 0x1F485, 0x1F4F7, 0x1F4F9, 0x1F4FA, 0x1F4FB,
+            0x1F30D, 0x1F30E, 0x1F310, 0x1F312, 0x1F316, 0x1F317, 0x1F318, 0x1F31A, 0x1F31C,
+            0x1F31D, 0x1F31E, 0x1F681, 0x1F682, 0x1F686, 0x1F688, 0x1F334, 0x1F335, 0x1F337,
+            0x1F338, 0x1F339, 0x1F33A, 0x1F33B, 0x1F33C, 0x1F33D, 0x1F33E, 0x1F33F, 0x1F340,
+            0x1F341, 0x1F342, 0x1F343, 0x1F344, 0x1F345, 0x1F346, 0x1F347, 0x1F348, 0x1F349,
+            0x1F34A, 0x1F34C, 0x1F34D, 0x1F34E, 0x1F34F, 0x1F351, 0x1F352, 0x1F353, 0x1F354,
+            0x1F355, 0x1F356, 0x1F357, 0x1F35A, 0x1F35B, 0x1F35C, 0x1F35D, 0x1F35E, 0x1F35F,
+            0x1F360, 0x1F361, 0x1F362, 0x1F363, 0x1F364, 0x1F365, 0x1F366, 0x1F367, 0x1F368,
+            0x1F369, 0x1F36A, 0x1F36B, 0x1F36C, 0x1F36D, 0x1F36E, 0x1F36F, 0x1F370, 0x1F371,
+            0x1F372, 0x1F373, 0x1F374, 0x1F375, 0x1F376, 0x1F377, 0x1F378, 0x1F379, 0x1F37A,
+            0x1F37B, 0x1F380, 0x1F381, 0x1F382, 0x1F383, 0x1F384, 0x1F385, 0x1F386, 0x1F387,
+            0x1F388, 0x1F389, 0x1F38A, 0x1F38B, 0x1F38C, 0x1F38D, 0x1F38E, 0x1F38F, 0x1F390,
+            0x1F391, 0x1F392, 0x1F393
+    };
+
+    static {
+        syms = new String[symsHex.length];
+        for (int i = 0; i < symsHex.length; i++) {
+            syms[i] = new String(Character.toChars(symsHex[i]));
+        }
+    }
+
+
+}
