a4e496a584527a81a65f1a81ab6780cb7ac13014
quaap <kliethermes.tom@gmail.com>
2017-03-14 22:38:36 -0500

    gpl3

--- app/src/main/java/quaap/com/dodatheexploda/SplashActivity.java
+++ app/src/main/java/quaap/com/dodatheexploda/SplashActivity.java
@@ -1,5 +1,20 @@
 package quaap.com.dodatheexploda;
 
+/**
+ * Copyright (C) 2017   Tom Kliethermes
+ *
+ * This file is part of DodaTheExploda and is is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ */
+
+
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
